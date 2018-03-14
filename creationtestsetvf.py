76fdd34d15e87ae25714aea6ad6a330f79ce47c1
edgartilly <32982317+edgartilly@users.noreply.github.com>
2018-03-14 10:51:34 +0100

    Update creationtestsetvf.py

--- creationtestsetvf.py
+++ creationtestsetvf.py
@@ -8,6 +8,8 @@ data2 = open('trainingset_done.json','r')
 clean_data1 = open('trainingset_testmodele.json','w')
 clean_data2 = open('trainingset_final.json','w')
 
+#create a random list with the range of the training set
+
 X = []
 for line in data1:
     text = json.loads(line)
@@ -16,18 +18,20 @@ for line in data1:
 liste = random.sample(range(0,5000),500)
 new_liste = sorted (liste)
 
-k = 0  # indice initial dans ta liste
+k = 0 
+
+#extraitre les tweets et les copier dans un autre fichier
 
 for line in data2:
     print ("I'm here")
-    tweet = json.loads(line) # tu charge la ligne dans la variable tweet
-    index = tweet['idx'] # tu charge l'index du tweet dans la variable index
+    tweet = json.loads(line) 
+    index = tweet['idx']
 
-    if X[new_liste[k]] == index: # tu verifies si l'element d'indice k de ta liste est egal a index; si oui:
-        json.dump(tweet, clean_data1) # tu copie la ligne dans ton training set
-        clean_data1.write("\n") # tu passe a la ligne dans ton training set
-        if k < 499: # tu evite de regarder apres l'element d'indice 4999, sinon ton programme bloque
-            k = k + 1 # tu passe a l'indice suivant dans ta liste
-    else: #si non:
-        json.dump(tweet, clean_data2) # tu copie la ligne dans ton test set
-        clean_data2.write("\n") # tu passe a la ligne dans ton test set
+    if X[new_liste[k]] == index:
+        json.dump(tweet, clean_data1) 
+        clean_data1.write("\n") #
+        if k < 499: 
+            k = k + 1
+    else:
+        json.dump(tweet, clean_data2) 
+        clean_data2.write("\n") 
